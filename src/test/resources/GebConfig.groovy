ced98f52155e8be3ca65c0c25afd846962c6bf17
Vlad <mclaren04@yandex.ru>
2019-07-14 20:02:12 +0300

    initial

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -1,50 +1,35 @@
-////driver = "htmlunit"
-//
-//import org.openqa.selenium.chrome.ChromeDriver
-//import org.openqa.selenium.chrome.ChromeDriverService
-//import org.openqa.selenium.os.ExecutableFinder
-//
-//import static org.apache.commons.lang3.SystemUtils.IS_OS_LINUX
-//import static org.apache.commons.lang3.SystemUtils.IS_OS_MAC
-//import static org.apache.commons.lang3.SystemUtils.IS_OS_WINDOWS
-//
-//File findDriverExecutable() {
-//    def defaultExecutable = new ExecutableFinder().find("chromedriver")
-//    if (defaultExecutable) {
-//        new File(defaultExecutable)
-//    } else {
-//        new File("drivers").listFiles().findAll {
-//            !it.name.endsWith(".version")
-//        }.find {
-//            if (IS_OS_LINUX) {
-//                it.name.contains("linux")
-//            } else if (IS_OS_MAC) {
-//                it.name.contains("mac")
-//            } else if (IS_OS_WINDOWS) {
-//                it.name.contains("windows")
-//            }
-//        }
-//    }
-//}
-//
-//driver = {
-//    ChromeDriverService.Builder serviceBuilder = new ChromeDriverService.Builder()
-//            .usingAnyFreePort()
-//            .usingDriverExecutable(findDriverExecutable())
-//    new ChromeDriver(serviceBuilder.build())
-//}
-//
-//baseUrl = "http://gebish.org"
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeDriverService
+import org.openqa.selenium.os.ExecutableFinder
 
-import org.openqa.selenium.firefox.FirefoxDriver
-import org.openqa.selenium.firefox.FirefoxOptions
+import static org.apache.commons.lang3.SystemUtils.IS_OS_LINUX
+import static org.apache.commons.lang3.SystemUtils.IS_OS_MAC
+import static org.apache.commons.lang3.SystemUtils.IS_OS_WINDOWS
 
-baseUrl = "http://gebish.org"
+File findDriverExecutable() {
+    def defaultExecutable = new ExecutableFinder().find("chromedriver")
+    if (defaultExecutable) {
+        new File(defaultExecutable)
+    } else {
+        new File("drivers").listFiles().findAll {
+            !it.name.endsWith(".version")
+        }.find {
+            if (IS_OS_LINUX) {
+                it.name.contains("linux")
+            } else if (IS_OS_MAC) {
+                it.name.contains("mac")
+            } else if (IS_OS_WINDOWS) {
+                it.name.contains("windows")
+            }
+        }
+    }
+}
 
 driver = {
-    FirefoxOptions options = new FirefoxOptions()
-    options.addArguments("incognito")
-    def firefoxDriver = new FirefoxDriver(options)
-    firefoxDriver.manage().window().maximize()
-    firefoxDriver
+    ChromeDriverService.Builder serviceBuilder = new ChromeDriverService.Builder()
+            .usingAnyFreePort()
+            .usingDriverExecutable(findDriverExecutable())
+    new ChromeDriver(serviceBuilder.build())
 }
+
+baseUrl = "http://gebish.org"
